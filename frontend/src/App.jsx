90a6c84c874d30547e480d560b8f366197fd3606
kavishwa-bhashitha <kaveebhashiofficial@gmail.com>
2024-02-21 23:55:35 +0200

    Django notes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
 import NotesListPage from "./pages/NotesListPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotePage from "./pages/NotePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <Router>
       <Header />
-      <NotesListPage />
-    </>
+      <Routes>
+        <Route path="/" element={<NotesListPage />} />
+        <Route path="/note/:id" element={<NotePage />} />
+      </Routes>
+    </Router>
   );
 }
 
